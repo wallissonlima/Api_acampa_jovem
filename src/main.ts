b2488951e23c971e527a17447e5bcbeca4d23a6b
Wallisson Lima <wallissonlima12@gmail.com>
2026-02-24 14:45:39 -0300

    ajustes para publicar

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { json, urlencoded } from 'express';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -23,8 +25,18 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
+  // 🔥 Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Acampa Jovem')
+    .setDescription('Documentação da API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   console.log(`🚀 API Acampa Jovem rodando na porta ${port}`);
 }
